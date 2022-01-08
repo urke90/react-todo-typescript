b5418076ff89ea58e21435a28dde786c03b7d92a
urke90 <urosbijelic90@gmail.com>
2022-01-08 18:28:02 +0100

    added context and remove todo logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,12 @@
-import { useState } from 'react';
-
 import Todos from './components/Todos';
-import Todo from './models/todo';
 import NewTodo from './components/NewTodo';
 
 function App() {
-    const [todos, setTodos] = useState<Todo[]>([]);
-
-    const addTodoHandler = (todoText: string) => {
-        const newTodo = new Todo(todoText);
-
-        setTodos((prevState) => prevState.concat(newTodo));
-    };
-
-    const removeTodoHandler = (id: string) => {
-        setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
-    };
-
     return (
-        <div>
-            <NewTodo onAddTodo={addTodoHandler} />
-            <Todos items={todos} onRemoveTodo={removeTodoHandler} />
-        </div>
+        <>
+            <NewTodo />
+            <Todos />
+        </>
     );
 }
 
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,21 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
-import Todo from '../models/todo';
 import TodoItem from './TodoItem';
+import { TodosContext } from '../store/todos-context';
 
 import classes from './Todos.module.css';
 
-const Todos: React.FC<{
-    items: Todo[];
-    onRemoveTodo: (id: string) => void;
-}> = ({ items, onRemoveTodo }) => {
+const Todos: React.FC = () => {
+    const todosCtx = useContext(TodosContext);
+
     return (
         <ul className={classes.todos}>
-            {items.map((item) => (
+            {todosCtx.items.map((item) => (
                 <TodoItem
                     key={item.id}
                     text={item.text}
-                    onRemoveTodo={onRemoveTodo.bind(null, item.id)}
+                    onRemoveTodo={todosCtx.removeTodo.bind(null, item.id)}
                 />
             ))}
         </ul>
